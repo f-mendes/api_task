9bb66950150d49b3f1a739fb8b8e28b855638602
Felipe <fsoares@autodoc.com.br>
2023-11-28 21:10:21 -0300

    feat: importing middlewares

---
+++ src/middlewares/json.js
@@ -0,0 +1,13 @@
+export async function json(req, res){
+  const buffer = []
+
+  for await (const chunck of req){
+    buffer.push(chunck)
+  }
+
+  try {
+		req.body = JSON.parse(Buffer.concat(buffer).toString())
+	} catch {
+		req.body = null
+	}
+}
