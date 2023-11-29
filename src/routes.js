66a17ff2f6380891f22149fb81ea6305a3b9e992
Felipe <fsoares@autodoc.com.br>
2023-11-28 23:52:27 -0300

    feat:creating rules for the methods

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,37 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from "./database.js"
 import { buildRoutePath } from "./utils/build-route-path.js"
+import { paramsRequired, idExist } from './utils/validate-params.js'
+import { datetime } from './utils/handle-date.js'
+
+const database = new Database()
 
 export const routes = [
   {
     method: 'POST',
     path: buildRoutePath('/tasks'),
     handle: (req, res) => {
-      console.log('POST')
+      const { title, description } = req.body
 
+      try {
+        paramsRequired(title, description)
+        const datetime = datetime()
+      
+        const task = {
+          id: randomUUID(),
+          title,
+          description,
+          completed_at: null,
+          created_at: datetime,
+          updated_at: datetime
+        }
+        
+        database.insert('tasks', task)
+
+      } catch (error) {
+        return res.writeHead(400).end(error)
+      }
+      
       return res.writeHead(201).end()
     }
   },
@@ -14,15 +39,47 @@ export const routes = [
     method: 'GET',
     path: buildRoutePath('/tasks'),
     handle: (req, res) => {
-      console.log('GET')
-      return res.writeHead(200).end()
+      const { search } = req.query
+
+      const users = database.select('tasks', search ? {
+        title: search,
+        description: search
+      } : null)
+
+			return res
+				.writeHead(200, {
+					'Content-Type': 'aplication/json'
+				})
+				.end(JSON.stringify(users))
     }
   },
   {
     method: 'PUT',
     path: buildRoutePath('/tasks/:id'),
     handle: (req, res) => {
-      console.log('PUT')
+      const { id } = req.params
+      const { title, description } = req.body
+
+      try {
+        const tasks = database.select('tasks', null)
+        idExist(id, tasks)
+        paramsRequired(title, description)
+
+        database.update('tasks', id, {
+          title,
+          description,
+          updated_at: datetime()
+        })
+
+      } catch (error) {
+
+        if (error === 'id não encontrado'){
+          return res.writeHead(404).end(error)
+        }
+
+        return res.writeHead(400).end(error)
+      }
+    
       return res.writeHead(204).end()
     }
   },
@@ -30,7 +87,18 @@ export const routes = [
     method: 'DELETE',
     path: buildRoutePath('/tasks/:id'),
     handle: (req, res) => {
-      console.log('DELETE')
+      const { id } = req.params
+
+      try {
+        const tasks = database.select('tasks', null)
+        idExist(id, tasks)
+        
+        database.delete('tasks', id)
+
+      } catch (error) {
+        return res.writeHead(404).end(error)
+      }
+
       return res.writeHead(204).end()
     }
   },
@@ -38,10 +106,23 @@ export const routes = [
     method: 'PATCH',
     path: buildRoutePath('/tasks/:id/complete'),
     handle: (req, res) => {
-      console.log('PATCH')
+      const { id } = req.params
+
+      try {
+        const tasks = database.select('tasks', null)
+        idExist(id, tasks)
+        
+        database.update('tasks', id, {
+          completed_at: true,
+          updated_at: datetime()
+        })
+
+      } catch (error) {
+        return res.writeHead(404).end(error)
+      }
       return res.writeHead(204).end()
     }
   },
-  
+
 
 ]
